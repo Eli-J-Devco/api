90b39ec0a4f42fa0348b04f7bbfc72899c9fb34e
lphamphoenixrs <69614018+lphamphoenixrs@users.noreply.github.com>
2023-01-06 11:42:44 +0700

    build test

--- src/main/java/com/nwm/api/config/WebSecurityConfig.java
+++ src/main/java/com/nwm/api/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 @EnableWebSecurity
@@ -41,6 +42,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/minisite")
                 .antMatchers("/minisite/**")
                 .antMatchers("/swagger-resources/**");
+    }
+    
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+            .allowedOrigins("https://staging.nextwavemonitoring.com")
+            .allowedMethods("PUT", "DELETE")
+            .allowedHeaders("header1", "header2", "header3")
+            .exposedHeaders("header1", "header2")
+            .allowCredentials(false).maxAge(3600);
     }
 
 }
