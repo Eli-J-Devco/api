888d9ca691bf17ab48439605cb5b73a0c1deea35
lphamphoenixrs <69614018+lphamphoenixrs@users.noreply.github.com>
2023-03-14 14:21:26 +0700

    Update CorsFilter.java

--- src/main/java/com/nwm/api/config/CorsFilter.java
+++ src/main/java/com/nwm/api/config/CorsFilter.java
@@ -26,7 +26,7 @@ public class CorsFilter implements Filter {
         final HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, grant_type, customer_type, client_id, lang, x-access-token, Access-Control-Allow-Origin");
+        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, grant_type, customer_type, client_id, lang, x-access-token");
         response.setHeader("Access-Control-Max-Age", "3600");
         
         if (HttpMethod.OPTIONS.name().equalsIgnoreCase(((HttpServletRequest) req).getMethod())) {
