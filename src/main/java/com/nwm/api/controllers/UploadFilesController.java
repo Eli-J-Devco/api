5ac422fdee8d66219c28f23905097736de2acb92
lphamphoenixrs <69614018+lphamphoenixrs@users.noreply.github.com>
2023-03-28 09:40:27 +0700

    Update UploadFilesController.java

--- src/main/java/com/nwm/api/controllers/UploadFilesController.java
+++ src/main/java/com/nwm/api/controllers/UploadFilesController.java
@@ -144,20 +144,20 @@ public class UploadFilesController extends BaseController {
 			@RequestParam(name = "FILETIME", required = false) String filetime) {
 
 //		public String message = " ";
-		System.out.println("---------------------------------start------------------------------");
-		System.out.println("SENDDATATRACE: " + senddatatrace);
-		System.out.println("MODE: " + mode);
-		System.out.println("SERIALNUMBER: " + serialnumber);
-		System.out.println("PASSWORD: " + password);
-		System.out.println("LOOPNAME: " + loopname);
-		System.out.println("MODBUSIP: " + modbusip);
-		System.out.println("MODBUSPORT: " + modbusport);
-		System.out.println("MODBUSDEVICE: " + modbusdevice);
-		System.out.println("MODBUSDEVICENAME: " + modbusdevicename);
-		System.out.println("MODBUSDEVICETYPE: " + modbusdevicetype);
-		System.out.println("MODBUSDEVICETYPENUMBER: " + modbusdevicetypenumber);
-		System.out.println("MODBUSDEVICECLASS: " + modbusdeviceclass);
-		System.out.println("-------------------------------end--------------------------------");
+//		System.out.println("---------------------------------start------------------------------");
+//		System.out.println("SENDDATATRACE: " + senddatatrace);
+//		System.out.println("MODE: " + mode);
+//		System.out.println("SERIALNUMBER: " + serialnumber);
+//		System.out.println("PASSWORD: " + password);
+//		System.out.println("LOOPNAME: " + loopname);
+//		System.out.println("MODBUSIP: " + modbusip);
+//		System.out.println("MODBUSPORT: " + modbusport);
+//		System.out.println("MODBUSDEVICE: " + modbusdevice);
+//		System.out.println("MODBUSDEVICENAME: " + modbusdevicename);
+//		System.out.println("MODBUSDEVICETYPE: " + modbusdevicetype);
+//		System.out.println("MODBUSDEVICETYPENUMBER: " + modbusdevicetypenumber);
+//		System.out.println("MODBUSDEVICECLASS: " + modbusdeviceclass);
+//		System.out.println("-------------------------------end--------------------------------");
 		
 		try {
 
