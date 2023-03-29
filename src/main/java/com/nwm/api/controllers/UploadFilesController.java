a1585dda7ef511709ca5b468a326e905437f21c0
lphamphoenixrs <69614018+lphamphoenixrs@users.noreply.github.com>
2023-03-29 08:27:22 +0700

    Build production

--- src/main/java/com/nwm/api/controllers/UploadFilesController.java
+++ src/main/java/com/nwm/api/controllers/UploadFilesController.java
@@ -1003,7 +1003,7 @@ public class UploadFilesController extends BaseController {
 //														}
 														
 														Path path = Paths.get(Lib.getReourcePropValue(Constants.appConfigFileName,
-																Constants.uploadRootPathConfigKey) + "/" + "bm-" + modbusdevice  + "-" + unique + "."
+																Constants.uploadRootPathConfigKey) + "/" + "bm-" +dataModelChint.getId_device()+ "-" + modbusdevice  + "-" + unique + "."
 																+ timeStamp + ".log.gz");
 														File logGzFile = new File(path.toString());
 														
