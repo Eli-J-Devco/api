aaaa0c08c143171fb31710aab257b9e59b6664f1
Hung Bui <hbui@nwemon.com>
2023-03-30 21:48:48 +0700

    Fix Task 696 - Site Config > Importing Data (comment at 2023-03-30)

--- src/main/java/com/nwm/api/services/ImportOldDataService.java
+++ src/main/java/com/nwm/api/services/ImportOldDataService.java
@@ -338,7 +338,7 @@ public class ImportOldDataService extends DB {
 			ObjectMapper oMapper = new ObjectMapper();
 			Map<String, String> map = oMapper.convertValue(dataList, Map.class);			
 			
-			String time =  map.get("time");
+			String time =  map.get("local_time");
 
 	    	if (time == null || !time.matches("((19|20)\\d\\d)-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01]) ([2][0-3]|[0-1][0-9]|[1-9]):[0-5][0-9]:([0-5][0-9]|[6][0])$")) {
 	    		set.clear();
