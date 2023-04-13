258a3b2b6aba8df46601c382e19288737f33740d
lphamphoenixrs <69614018+lphamphoenixrs@users.noreply.github.com>
2023-04-13 14:28:45 +0700

    Build staging server

--- src/main/java/com/nwm/api/controllers/UploadFilesController.java
+++ src/main/java/com/nwm/api/controllers/UploadFilesController.java
@@ -1468,9 +1468,9 @@ public class UploadFilesController extends BaseController {
 												List<String> words = Lists.newArrayList(Splitter.on(',').split(line));
 												if (words.size() > 0) {
 													DeviceEntity deviceUpdateE = new DeviceEntity();
-													if(!Lib.isBlank(words.get(36))) {
+													if(!Lib.isBlank(words.get(25))) {
 														deviceUpdateE.setLast_updated(words.get(0).replace("'", ""));
-														deviceUpdateE.setLast_value(!Lib.isBlank(words.get(36)) ? Double.parseDouble(words.get(36)) : null);
+														deviceUpdateE.setLast_value(!Lib.isBlank(words.get(25)) ? Double.parseDouble(words.get(25)) : null);
 													} else {
 														deviceUpdateE.setLast_updated(null);
 														deviceUpdateE.setLast_value(null);
