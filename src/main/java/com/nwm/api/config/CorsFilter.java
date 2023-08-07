1f40a189f9b59d4d9e6b53043c48283c439ba7eb
lphamphoenixrs <69614018+lphamphoenixrs@users.noreply.github.com>
2023-08-07 16:45:56 +0700

    Update CorsFilter.java

--- src/main/java/com/nwm/api/config/CorsFilter.java
+++ src/main/java/com/nwm/api/config/CorsFilter.java
@@ -25,6 +25,10 @@ public class CorsFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         final HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
+        System.out.println("req: " + req);
+        
+        System.out.println("req: " + res);
+        
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, grant_type, customer_type, client_id, lang, x-access-token");
