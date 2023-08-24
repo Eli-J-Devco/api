57d636db872b9bfd56be0543909c4910857f829b
lphamphoenixrs <69614018+lphamphoenixrs@users.noreply.github.com>
2023-08-25 00:49:15 +0700

    Update CustomUserDetailService.java

--- src/main/java/com/nwm/api/services/CustomUserDetailService.java
+++ src/main/java/com/nwm/api/services/CustomUserDetailService.java
@@ -11,6 +11,9 @@ import java.util.Collection;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -29,7 +32,7 @@ public class CustomUserDetailService implements UserDetailsService {
     private BCryptPasswordEncoder passwordEncoder;
     
     @Override
-    public UserDetails loadUserByUsername(String username) throws InvalidGrantException {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
     	userService = new UserService();
     	employeeService = new EmployeeService();
     	String[] split = username.split(":");
@@ -48,25 +51,26 @@ public class CustomUserDetailService implements UserDetailsService {
     		user = userService.getUserByUserName(usernameLogin);
     	}
     	String message = "";
-    	
-    	// Update lock account
-    	UserEntity userEn = new UserEntity();
-    	userEn.setId(user.getId());
     	double timeAccountLocked = user.getTime_account_locked() > 0 ? user.getTime_account_locked() : 1;
     	int maxFailedAttempt = user.getMax_failed_attempt() > 0 ? user.getMax_failed_attempt() : 6;
-    	
-    	Date date = new Date();
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		userEn.setLock_time(format.format(date).toString());
-    	
-    	userEn.setFailed_attempt( user.getFailed_attempt() >= maxFailedAttempt ? maxFailedAttempt: (user.getFailed_attempt() + 1));
-    	if( (user.getFailed_attempt() + 1) >= maxFailedAttempt) { userEn.setAccount_locked(1); }
-    	employeeService.updateLockAccount(userEn);
     	
     	if (user.getId() == 0) {
     		message = "The account does not exist or has been locked. Please contact administrator.";
             throw new InvalidGrantException(message);
         } else if(user.getAccount_locked() == 1 || user.getFailed_attempt() >= maxFailedAttempt) {
+        	// Update lock account
+        	UserEntity userEn = new UserEntity();
+        	userEn.setId(user.getId());
+        	
+        	
+        	Date date = new Date();
+    		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    		userEn.setLock_time(format.format(date).toString());
+        	
+        	userEn.setFailed_attempt( user.getFailed_attempt() >= maxFailedAttempt ? maxFailedAttempt: (user.getFailed_attempt() + 1));
+        	if( (user.getFailed_attempt() + 1) >= maxFailedAttempt) { userEn.setAccount_locked(1); }
+        	employeeService.updateLockAccount(userEn);
+        	
 			if(timeAccountLocked < 1 && timeAccountLocked > 0) {
 				message = "Your account has been locked due to "+maxFailedAttempt+" failed attempts. It will be unlocked after " + (int)(timeAccountLocked * 60) + " minute"+ ( (timeAccountLocked * 60) > 1 ? "s": "") + ".";
 			} else {
@@ -87,4 +91,5 @@ public class CustomUserDetailService implements UserDetailsService {
 
 		return user;
     }
+    
 }
