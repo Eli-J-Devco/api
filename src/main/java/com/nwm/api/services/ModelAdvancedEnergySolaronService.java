8f3fbadd637c6a0d1983fd9c17f1f1778aab1130
lphamphoenixrs <69614018+lphamphoenixrs@users.noreply.github.com>
2023-10-06 16:16:10 +0700

    Build production server fix alert on AE333NX Inverter

--- src/main/java/com/nwm/api/services/ModelAdvancedEnergySolaronService.java
+++ src/main/java/com/nwm/api/services/ModelAdvancedEnergySolaronService.java
@@ -129,9 +129,9 @@ public class ModelAdvancedEnergySolaronService extends DB {
 	        ZonedDateTime zdtNowLosAngeles = ZonedDateTime.now(zoneIdLosAngeles);
 	        int hours = zdtNowLosAngeles.getHour();
 	        
-	        if(hours >=9 && hours <= 17) {
+//	        if(hours >=9 && hours <= 17) {
 	        	checkTriggerAlertModelAdvancedEnergySolaron(obj);
-	        }
+//	        }
 			
 			return true;
 		} catch (Exception ex) {
