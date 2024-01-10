9e2f4d81c21e2ccc96c18d70ee58dcd5e5e2f2f9
Long <lpham@phoenixrs.com>
2024-01-10 23:59:37 +0700

    Update ModelLeviton70D48000Service.java

--- src/main/java/com/nwm/api/services/ModelLeviton70D48000Service.java
+++ src/main/java/com/nwm/api/services/ModelLeviton70D48000Service.java
@@ -50,7 +50,7 @@ public class ModelLeviton70D48000Service extends DB {
 				dataModel70D48.setVoltageL2L3(Double.parseDouble(!Lib.isBlank(words.get(14)) ? words.get(14) : "0.001"));
 				dataModel70D48.setVoltageL3L1(Double.parseDouble(!Lib.isBlank(words.get(15)) ? words.get(15) : "0.001"));
 				dataModel70D48.setLineFrequency(Double.parseDouble(!Lib.isBlank(words.get(16)) ? words.get(16) : "0.001"));
-				dataModel70D48.setPowerSum(power);
+				dataModel70D48.setPowerSum (power);
 				dataModel70D48.setPowerCH1A(Double.parseDouble(!Lib.isBlank(words.get(18)) ? words.get(18) : "0.001"));
 				dataModel70D48.setPowerCH2B(Double.parseDouble(!Lib.isBlank(words.get(19)) ? words.get(19) : "0.001"));
 				dataModel70D48.setPowerCH3C(Double.parseDouble(!Lib.isBlank(words.get(20)) ? words.get(20) : "0.001"));
@@ -111,7 +111,7 @@ public class ModelLeviton70D48000Service extends DB {
 				
 				// set custom field nvmActivePower and nvmActiveEnergy
 				dataModel70D48.setNvmActivePower(power);
-				dataModel70D48.setNvmActiveEnergy(Double.parseDouble(!Lib.isBlank(words.get(33)) ? words.get(33) : "0.001"));
+				dataModel70D48.setNvmActiveEnergy(Double.parseDouble(!Lib.isBlank(words.get(37)) ? words.get(37) : "0.001"));
 				
 				return dataModel70D48;
 				
