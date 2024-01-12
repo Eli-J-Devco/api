80cdeb299d31758eda5b56244fbea589832e2e82
Long <lpham@phoenixrs.com>
2024-01-12 15:41:23 +0700

    Update CustomTokenEnhancer.java

--- src/main/java/com/nwm/api/config/CustomTokenEnhancer.java
+++ src/main/java/com/nwm/api/config/CustomTokenEnhancer.java
@@ -37,7 +37,7 @@ implements TokenEnhancer {
         		additionalInfo.put("id_sites", user.getId_sites());
         		additionalInfo.put("is_technical", user.getIs_technical());
         		additionalInfo.put("table_column", user.getTable_column());
-//        		additionalInfo.put("id_company", user.getId_company());
+        		additionalInfo.put("id_company", user.getId_company());
         		
         	}
         	
