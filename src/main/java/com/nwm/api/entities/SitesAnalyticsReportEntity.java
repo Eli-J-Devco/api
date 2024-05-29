a60586342791aaee6f0751bebcf6a42a3eda3886
nartnahnnw <nhantran@nwemon.com>
2024-05-29 09:06:18 +0700

    Add getter, setter for SitesAnalyticsReportEntity

--- src/main/java/com/nwm/api/entities/SitesAnalyticsReportEntity.java
+++ src/main/java/com/nwm/api/entities/SitesAnalyticsReportEntity.java
@@ -1,10 +1,5 @@
 package com.nwm.api.entities;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class SitesAnalyticsReportEntity {
     private int id;
     private String siteName;
@@ -23,6 +18,70 @@ public class SitesAnalyticsReportEntity {
         this.fileName = fileName;
         this.fileType = fileType;
         this.email = email;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSiteName() {
+        return siteName;
+    }
+
+    public void setSiteName(String siteName) {
+        this.siteName = siteName;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getHtml() {
+        return html;
+    }
+
+    public void setHtml(String html) {
+        this.html = html;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
         this.title = title;
     }
 }
