f76ac634bc89eb343784642a42d57bf3ce303bdd
Long <lpham@phoenixrs.com>
2025-01-04 17:16:47 +0700

    Update ModelShark250Service.java

--- src/main/java/com/nwm/api/services/ModelShark250Service.java
+++ src/main/java/com/nwm/api/services/ModelShark250Service.java
@@ -29,7 +29,7 @@ public class ModelShark250Service extends DB {
 				ModelShark250Entity dataModelShark250 = new ModelShark250Entity();
 				
 				Double power = Double.parseDouble(!Lib.isBlank(words.get(4)) ? words.get(4) : "0.001");
-				Double energy = Double.parseDouble(!Lib.isBlank(words.get(12)) ? words.get(12) : "0.001");
+				Double energy = Double.parseDouble(!Lib.isBlank(words.get(9)) ? words.get(9) : "0.001");
 				if(energy < 0 && offset_data_old > 0) {
 					energy = energy * -1;
 					energy = (energy + offset_data_old) * -1;
@@ -48,11 +48,11 @@ public class ModelShark250Service extends DB {
 				dataModelShark250.setApparentPower(Double.parseDouble(!Lib.isBlank(words.get(6)) ? words.get(6) : "0.001"));
 				dataModelShark250.setActiveEnergyReceived(Double.parseDouble(!Lib.isBlank(words.get(7)) ? words.get(7) : "0.001"));
 				dataModelShark250.setActiveEnergyDelivered(Double.parseDouble(!Lib.isBlank(words.get(8)) ? words.get(8) : "0.001"));
-				dataModelShark250.setActiveEnergyNet(Double.parseDouble(!Lib.isBlank(words.get(9)) ? words.get(9) : "0.001"));
+				dataModelShark250.setActiveEnergyNet(energy);
 				dataModelShark250.setReactiveEnergyReceived(Double.parseDouble(!Lib.isBlank(words.get(10)) ? words.get(10) : "0.001"));
 				
 				dataModelShark250.setReactiveEnergyDelivered(Double.parseDouble(!Lib.isBlank(words.get(11)) ? words.get(11) : "0.001"));
-				dataModelShark250.setReactiveEnergyNet(energy);
+				dataModelShark250.setReactiveEnergyNet(Double.parseDouble(!Lib.isBlank(words.get(12)) ? words.get(12) : "0.001"));
 				dataModelShark250.setApparentEnergy(Double.parseDouble(!Lib.isBlank(words.get(13)) ? words.get(13) : "0.001"));
 				dataModelShark250.setFrequency(Double.parseDouble(!Lib.isBlank(words.get(14)) ? words.get(14) : "0.001"));
 				dataModelShark250.setPowerFactor(Double.parseDouble(!Lib.isBlank(words.get(15)) ? words.get(15) : "0.001"));
