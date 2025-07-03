3d80ee373d54667c3cef5b75d7984b84642b4c53
Long <lpham@phoenixrs.com>
2025-07-03 10:29:52 +0700

    Fix driver Lovato, SMA

--- src/main/java/com/nwm/api/services/ModelSmaShp7510Service.java
+++ src/main/java/com/nwm/api/services/ModelSmaShp7510Service.java
@@ -114,6 +114,11 @@ public class ModelSmaShp7510Service extends DB {
 		try {
 			ModelSmaShp7510Entity dataObj = (ModelSmaShp7510Entity) queryForObject("ModelSmaShp7510.getLastRow", obj);
 			
+			// filter data 
+			if(dataObj != null && ( obj.getError() > 0 || obj.getNvmActiveEnergy() < dataObj.getNvmActiveEnergy() || obj.getNvmActiveEnergy() == 0.001 || obj.getNvmActiveEnergy() < 0) ) {
+				obj.setNvmActiveEnergy(dataObj.getNvmActiveEnergy());
+				obj.setTotalyield(dataObj.getNvmActiveEnergy());
+			}
 			 double measuredProduction = 0;
 			 if(dataObj != null && dataObj.getId_device() > 0 && dataObj.getNvmActiveEnergy() > 0 && obj.getNvmActiveEnergy() > 0 && obj.getNvmActiveEnergy() != 0.001 ) {
 				 measuredProduction = obj.getNvmActiveEnergy() - dataObj.getNvmActiveEnergy();
