86168226a8f74e237ec9e60fbd3eba70d44cb7e1
Hung Bui <hbui@nwemon.com>
2025-08-18 09:42:53 +0700

    Feat: add audit logging for report

--- src/main/java/com/nwm/api/entities/ReportDuplicateRequest.java
+++ src/main/java/com/nwm/api/entities/ReportDuplicateRequest.java
@@ -8,6 +8,7 @@ package com.nwm.api.entities;
 public class ReportDuplicateRequest {
 	private int id;
 	private int newId;
+	private Integer created_by;
 	
 	public int getId() {
 		return id;
@@ -20,5 +21,11 @@ public class ReportDuplicateRequest {
 	}
 	public void setNewId(int newId) {
 		this.newId = newId;
+	}
+	public Integer getCreated_by() {
+		return created_by;
+	}
+	public void setCreated_by(Integer created_by) {
+		this.created_by = created_by;
 	}
 }
