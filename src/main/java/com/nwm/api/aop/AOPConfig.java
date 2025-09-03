dd96fabd61faabbf7d593eadf83108c441e02f2e
Hung Bui <hbui@nwemon.com>
2025-09-03 20:01:53 +0700

    Refactor (account status): using AOP for logging

---
+++ src/main/java/com/nwm/api/aop/AOPConfig.java
@@ -0,0 +1,16 @@
+package com.nwm.api.aop;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AOPConfig {
+
+	@Bean
+	AccountStatusAOP accountStatusAOP() {
+		return new AccountStatusAOP();
+	}
+	   
+}
