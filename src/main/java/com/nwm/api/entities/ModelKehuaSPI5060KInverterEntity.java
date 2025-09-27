66ddd482916d7376e999808a7956c45829360602
Long <lpham@phoenixrs.com>
2025-09-27 18:48:58 +0700

    Create ModelKehuaSPI5060KInverterEntity.java

---
+++ src/main/java/com/nwm/api/entities/ModelKehuaSPI5060KInverterEntity.java
@@ -0,0 +1,182 @@
+package com.nwm.api.entities;
+
+public class ModelKehuaSPI5060KInverterEntity extends ModelBaseEntity {
+	private double DeviceStatus;
+	private double DailyEnergy;
+	private double TotalEnergy;
+	private double Gridfrequency;
+	private double UphaseUVgridvoltage;
+	private double VphaseVWgridvoltage;
+	private double WphaseWUgridvoltage;
+	private double Uphasegridcurrent;
+	private double Vphasegridcurrent;
+	private double Wphasegridcurrent;
+	private double Gridconnectedtotalactivepower;
+	private double Gridconnectedtotalreactivepower;
+	private double Heatsinktemperature;
+	private double Innertemperature;
+	private double Gridconnectedtotalapparentpower;
+	private double IGBTtemperature;
+	private double Outputpowerfactor;
+	private double PVinputtotalpower;
+	private double ACleakagecurrent;
+	private double Dailypowerconsumption;
+	private double Totalpowerconsumption;
+	private double Ongridactivepower;
+	private double Ongridapparentpower;
+	private double Ongridreactivepower;
+	private double OngridPowerfactor;
+	public double getDeviceStatus() {
+		return DeviceStatus;
+	}
+	public void setDeviceStatus(double deviceStatus) {
+		DeviceStatus = deviceStatus;
+	}
+	public double getDailyEnergy() {
+		return DailyEnergy;
+	}
+	public void setDailyEnergy(double dailyEnergy) {
+		DailyEnergy = dailyEnergy;
+	}
+	public double getTotalEnergy() {
+		return TotalEnergy;
+	}
+	public void setTotalEnergy(double totalEnergy) {
+		TotalEnergy = totalEnergy;
+	}
+	public double getGridfrequency() {
+		return Gridfrequency;
+	}
+	public void setGridfrequency(double gridfrequency) {
+		Gridfrequency = gridfrequency;
+	}
+	public double getUphaseUVgridvoltage() {
+		return UphaseUVgridvoltage;
+	}
+	public void setUphaseUVgridvoltage(double uphaseUVgridvoltage) {
+		UphaseUVgridvoltage = uphaseUVgridvoltage;
+	}
+	public double getVphaseVWgridvoltage() {
+		return VphaseVWgridvoltage;
+	}
+	public void setVphaseVWgridvoltage(double vphaseVWgridvoltage) {
+		VphaseVWgridvoltage = vphaseVWgridvoltage;
+	}
+	public double getWphaseWUgridvoltage() {
+		return WphaseWUgridvoltage;
+	}
+	public void setWphaseWUgridvoltage(double wphaseWUgridvoltage) {
+		WphaseWUgridvoltage = wphaseWUgridvoltage;
+	}
+	public double getUphasegridcurrent() {
+		return Uphasegridcurrent;
+	}
+	public void setUphasegridcurrent(double uphasegridcurrent) {
+		Uphasegridcurrent = uphasegridcurrent;
+	}
+	public double getVphasegridcurrent() {
+		return Vphasegridcurrent;
+	}
+	public void setVphasegridcurrent(double vphasegridcurrent) {
+		Vphasegridcurrent = vphasegridcurrent;
+	}
+	public double getWphasegridcurrent() {
+		return Wphasegridcurrent;
+	}
+	public void setWphasegridcurrent(double wphasegridcurrent) {
+		Wphasegridcurrent = wphasegridcurrent;
+	}
+	public double getGridconnectedtotalactivepower() {
+		return Gridconnectedtotalactivepower;
+	}
+	public void setGridconnectedtotalactivepower(double gridconnectedtotalactivepower) {
+		Gridconnectedtotalactivepower = gridconnectedtotalactivepower;
+	}
+	public double getGridconnectedtotalreactivepower() {
+		return Gridconnectedtotalreactivepower;
+	}
+	public void setGridconnectedtotalreactivepower(double gridconnectedtotalreactivepower) {
+		Gridconnectedtotalreactivepower = gridconnectedtotalreactivepower;
+	}
+	public double getHeatsinktemperature() {
+		return Heatsinktemperature;
+	}
+	public void setHeatsinktemperature(double heatsinktemperature) {
+		Heatsinktemperature = heatsinktemperature;
+	}
+	public double getInnertemperature() {
+		return Innertemperature;
+	}
+	public void setInnertemperature(double innertemperature) {
+		Innertemperature = innertemperature;
+	}
+	public double getGridconnectedtotalapparentpower() {
+		return Gridconnectedtotalapparentpower;
+	}
+	public void setGridconnectedtotalapparentpower(double gridconnectedtotalapparentpower) {
+		Gridconnectedtotalapparentpower = gridconnectedtotalapparentpower;
+	}
+	public double getIGBTtemperature() {
+		return IGBTtemperature;
+	}
+	public void setIGBTtemperature(double iGBTtemperature) {
+		IGBTtemperature = iGBTtemperature;
+	}
+	public double getOutputpowerfactor() {
+		return Outputpowerfactor;
+	}
+	public void setOutputpowerfactor(double outputpowerfactor) {
+		Outputpowerfactor = outputpowerfactor;
+	}
+	public double getPVinputtotalpower() {
+		return PVinputtotalpower;
+	}
+	public void setPVinputtotalpower(double pVinputtotalpower) {
+		PVinputtotalpower = pVinputtotalpower;
+	}
+	public double getACleakagecurrent() {
+		return ACleakagecurrent;
+	}
+	public void setACleakagecurrent(double aCleakagecurrent) {
+		ACleakagecurrent = aCleakagecurrent;
+	}
+	public double getDailypowerconsumption() {
+		return Dailypowerconsumption;
+	}
+	public void setDailypowerconsumption(double dailypowerconsumption) {
+		Dailypowerconsumption = dailypowerconsumption;
+	}
+	public double getTotalpowerconsumption() {
+		return Totalpowerconsumption;
+	}
+	public void setTotalpowerconsumption(double totalpowerconsumption) {
+		Totalpowerconsumption = totalpowerconsumption;
+	}
+	public double getOngridactivepower() {
+		return Ongridactivepower;
+	}
+	public void setOngridactivepower(double ongridactivepower) {
+		Ongridactivepower = ongridactivepower;
+	}
+	public double getOngridapparentpower() {
+		return Ongridapparentpower;
+	}
+	public void setOngridapparentpower(double ongridapparentpower) {
+		Ongridapparentpower = ongridapparentpower;
+	}
+	public double getOngridreactivepower() {
+		return Ongridreactivepower;
+	}
+	public void setOngridreactivepower(double ongridreactivepower) {
+		Ongridreactivepower = ongridreactivepower;
+	}
+	public double getOngridPowerfactor() {
+		return OngridPowerfactor;
+	}
+	public void setOngridPowerfactor(double ongridPowerfactor) {
+		OngridPowerfactor = ongridPowerfactor;
+	}
+	
+	
+	
+}
