96cf71ea6ec0024b548109a03fb4c6d706487301
Hung Bui <hbui@nwemon.com>
2025-11-21 14:43:45 +0700

    Fix (HVAC BEMS): fix datetime list

--- src/main/java/com/nwm/api/services/EnergyUsageService.java
+++ src/main/java/com/nwm/api/services/EnergyUsageService.java
@@ -59,37 +59,24 @@ public class EnergyUsageService extends DB {
 							timeFullFormat = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm");
 							categoriesTimeFormat = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm");
 						}
-                        if ( dataEnergyUsage != null && !dataEnergyUsage.isEmpty()) {
-                            start = LocalDateTime.parse(dataEnergyUsage.get(0).getTime_full(), timeFullFormat);
-                        }
 						break;
 					case "day":
 						interval = 1;
 						timeUnit = ChronoUnit.DAYS;
 						timeFullFormat = DateTimeFormatter.ofPattern("MM-dd-yyyy");
 						categoriesTimeFormat = DateTimeFormatter.ofPattern("dd. LLL");
-                        if ( dataEnergyUsage != null && !dataEnergyUsage.isEmpty()) {
-                            start = LocalDate.parse(dataEnergyUsage.get(0).getTime_full(), timeFullFormat).atStartOfDay();
-                        }
 						break;
 					case "month":
 						interval = 1;
 						timeUnit = ChronoUnit.MONTHS;
 						timeFullFormat = DateTimeFormatter.ofPattern("MM-yyyy");
 						categoriesTimeFormat = DateTimeFormatter.ofPattern("LLL. yyyy");
-                        if ( dataEnergyUsage != null && !dataEnergyUsage.isEmpty()) {
-                            YearMonth ym = YearMonth.parse(dataEnergyUsage.get(0).getTime_full(), timeFullFormat);
-                            start = ym.atDay(1).atStartOfDay();
-                        }
 						break;
 					default:
 						interval = 1;
 						timeUnit = ChronoUnit.DAYS;
 						timeFullFormat = DateTimeFormatter.ofPattern("MM-dd-yyyy");
 						categoriesTimeFormat = DateTimeFormatter.ofPattern("dd. LLL");
-                        if ( dataEnergyUsage != null && !dataEnergyUsage.isEmpty()) {
-                            start = LocalDate.parse(dataEnergyUsage.get(0).getTime_full(), timeFullFormat).atStartOfDay();
-                        }
 						break;
 				}
 				
