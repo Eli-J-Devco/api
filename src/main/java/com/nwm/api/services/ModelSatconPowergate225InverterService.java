19a3bd9f52f66f43f62f5555c8ebf32354facd4e
Hung Bui <hbui@nwemon.com>
2026-01-14 14:53:19 +0700

    Refactor (upload service): remove boilerplate code

--- src/main/java/com/nwm/api/services/ModelSatconPowergate225InverterService.java
+++ src/main/java/com/nwm/api/services/ModelSatconPowergate225InverterService.java
@@ -113,9 +113,9 @@ public class ModelSatconPowergate225InverterService extends DB {
 				return false;
 			}
 			
-			ZoneId zoneIdLosAngeles = ZoneId.of("America/Los_Angeles"); // "America/Los_Angeles"
-	        ZonedDateTime zdtNowLosAngeles = ZonedDateTime.now(zoneIdLosAngeles);
-	        int hours = zdtNowLosAngeles.getHour();
+			ZoneId zoneId = ZoneId.of(obj.getTimezone_value());
+			ZonedDateTime zdtNow = ZonedDateTime.now(zoneId);
+			int hours = zdtNow.getHour();
 	        
 	        if (hours >= 9 && hours <= 17 && dataObj.getEnable_alert() >= 1) {
 	        	checkTriggerAlertModelSatconPVS357Inverter(obj);
