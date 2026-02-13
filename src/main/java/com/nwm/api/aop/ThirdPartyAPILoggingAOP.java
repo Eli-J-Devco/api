119b8830c689d85ccd0a45844542eff7ef9a54c6
Hung Bui <hbui@nwemon.com>
2026-02-13 10:38:41 +0700

    Perf (third party api logging): replace security key check by checking unauthorized status

--- src/main/java/com/nwm/api/aop/ThirdPartyAPILoggingAOP.java
+++ src/main/java/com/nwm/api/aop/ThirdPartyAPILoggingAOP.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpServletRequest;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -18,9 +20,13 @@ public class ThirdPartyAPILoggingAOP {
 	@Pointcut("within(com.nwm.api.controllers.ThirdPartyAPIController)")
 	public void thirdPartyAPIMethod() {}
 	
-	@AfterReturning("thirdPartyAPIMethod()")
-    public void afterSiteDetailMethod() {
+	@AfterReturning(value = "thirdPartyAPIMethod()", returning = "result")
+    public void afterSiteDetailMethod(Object result) {
 		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+		if (result instanceof ResponseEntity) {
+			if (((ResponseEntity<?>) result).getStatusCode().compareTo(HttpStatus.UNAUTHORIZED) == 0) return;
+		}
+
 		String route = request.getRequestURI().substring(request.getContextPath().length());
 		String method = request.getMethod();
 		String security_key = request.getHeader("X-NWM-API-KEY");
