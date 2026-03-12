82072930c1708f4838ca6d4753eecd9c4828c065
quan.nguyen <q.nguyen@nwemon.com>
2026-03-12 15:03:00 +0700

    add rate limit per min

--- src/main/java/com/nwm/api/entities/APIAccessLoggingDTO.java
+++ src/main/java/com/nwm/api/entities/APIAccessLoggingDTO.java
@@ -9,12 +9,20 @@ public class APIAccessLoggingDTO {
 	private String route;
 	private String method;
 	private String security_key;
+    private int checkByMinute;
 	
 	public APIAccessLoggingDTO(String route, String method, String security_key) {
 		this.route = route;
 		this.method = method;
 		this.security_key = security_key;
 	}
+
+    public APIAccessLoggingDTO(String route, String method, String security_key, int checkByMinute) {
+        this.route = route;
+        this.method = method;
+        this.security_key = security_key;
+        this.checkByMinute = checkByMinute;
+    }
 	
 	public String getRoute() {
 		return route;
@@ -34,5 +42,12 @@ public class APIAccessLoggingDTO {
 	public void setSecurity_key(String security_key) {
 		this.security_key = security_key;
 	}
-	
+
+    public int getCheckByMinute() {
+        return checkByMinute;
+    }
+
+    public void setCheckByMinute(int checkByMinute) {
+        this.checkByMinute = checkByMinute;
+    }
 }
