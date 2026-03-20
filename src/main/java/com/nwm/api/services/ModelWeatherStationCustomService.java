be3839c323a2feef5578fe0f1ca6bdae9cb525b8
Long <lpham@phoenixrs.com>
2026-03-20 09:17:08 +0700

    Update ModelWeatherStationCustomService.java

--- src/main/java/com/nwm/api/services/ModelWeatherStationCustomService.java
+++ src/main/java/com/nwm/api/services/ModelWeatherStationCustomService.java
@@ -8,12 +8,15 @@ package com.nwm.api.services;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
 import com.nwm.api.DBManagers.DB;
 import com.nwm.api.entities. ModelWeatherStationCustomEntity;
 import com.nwm.api.utils.Lib;
 
+@Service
 public class ModelWeatherStationCustomService extends DB {
 	/**
 	 * @description set data  ModelWeatherStationCustom
