599235807962ec1b20669654d8af173ae9c4cd9f
Long <lpham@phoenixrs.com>
2026-03-23 08:49:45 +0700

    Add test generation energy data.

---
+++ src/main/java/com/nwm/api/config/BatchGenerationEnergy.java
@@ -0,0 +1,33 @@
+/********************************************************
+ * Copyright 2020-2021 NEXT WAVE ENERGY MONITORING INC.
+ * All rights reserved.
+ *
+ *********************************************************/
+package com.nwm.api.config;
+
+import com.nwm.api.batchjob.BatchJobGenerationEnergy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+@ConditionalOnProperty(
+        name = "datalogger.cronjob.active",
+        havingValue = "true",
+        matchIfMissing = false
+)
+public class BatchGenerationEnergy {
+    @Autowired
+    BatchJobGenerationEnergy batchJobGenerationEnergy;
+
+    private final boolean isFirstRun = true;
+
+//    @Scheduled(cron = "*/1 * * * * ?")
+//    public void generationEnergyData()  {
+//        batchJobGenerationEnergy.generationEnergyData();
+//    }
+}
