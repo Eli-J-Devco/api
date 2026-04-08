3152fd392e8ab118580b1b7b9966582c1efa69e4
Minh <mle@nwemon.com>
2026-04-08 16:59:27 +0700

    WEBDEV-4749 : Site Map Tab – Inverter-Level Map, Device Metrics Selection, and Tooltip Redesign

--- src/main/java/com/nwm/api/entities/SiteEntity.java
+++ src/main/java/com/nwm/api/entities/SiteEntity.java
@@ -340,6 +340,7 @@ public class SiteEntity extends SortEntity {
     private String postgres_table;
     private String data_weather;
     private int is_day;
+	private int site_map;
     
     
 	
@@ -2224,4 +2225,12 @@ public class SiteEntity extends SortEntity {
     public void setPostgres_table(String postgres_table) {
         this.postgres_table = postgres_table;
     }
+
+	public int getSite_map() {
+		return site_map;
+	}
+
+	public void setSite_map(int site_map) {
+		this.site_map = site_map;
+	}
 }
