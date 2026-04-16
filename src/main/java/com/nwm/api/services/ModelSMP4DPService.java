eb1b2dbe531e42075b34c451850dcd80ec2ef70f
Long <lpham@phoenixrs.com>
2026-04-16 08:47:06 +0700

    Update ModelSMP4DPService.java

--- src/main/java/com/nwm/api/services/ModelSMP4DPService.java
+++ src/main/java/com/nwm/api/services/ModelSMP4DPService.java
@@ -625,7 +625,7 @@ public class ModelSMP4DPService extends DB {
 			ZonedDateTime zdtNow = ZonedDateTime.now(zoneId);
 			int hours = zdtNow.getHour();
 			if (hours >= 9 && hours <= 17 && obj.getEnable_alert() >= 1) {
-                service.checkTriggerAlert(obj.getDatatablename(), obj.getTime(), obj.getId_device(), AlertEnum.values());
+//                service.checkTriggerAlert(obj.getDatatablename(), obj.getTime(), obj.getId_device(), AlertEnum.values());
 			}
 	        return true;
 		} catch (Exception ex) {
