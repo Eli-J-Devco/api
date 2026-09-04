adb379bda86fc64116fe2fe59e3f67d811b96131
Hung Bui <hbui@nwemon.com>
2026-09-04 20:25:02 +0700

    Feat: Add send mail api for admin system announcement

--- src/main/java/com/nwm/api/controllers/PlatformStatusController.java
+++ src/main/java/com/nwm/api/controllers/PlatformStatusController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nwm.api.entities.EmailAnnouncementRequest;
 import com.nwm.api.entities.SystemAnnouncementEntity;
 import com.nwm.api.services.PlatformStatusService;
 import com.nwm.api.utils.Constants;
@@ -46,5 +47,16 @@ public class PlatformStatusController extends BaseController {
 	@PostMapping("/system-announcement/save")
 	public Object saveSystemAnnouncement(@RequestBody SystemAnnouncementEntity obj) {
 		return service.saveSystemAnnouncement(obj) ? this.jsonResult(true, Constants.SAVE_SUCCESS_MSG, obj, 1) : this.jsonResult(false, Constants.SAVE_ERROR_MSG, null, 0);
+	}
+	
+	/**
+	 * @description send mail system announcement
+	 * @author Hung.Bui
+	 * @since 2026-09-04
+	 * @param obj
+	 */
+	@PostMapping("/system-announcement/send-mail")
+	public Object sendMailSystemAnnouncement(@RequestBody EmailAnnouncementRequest obj) {
+		return service.sendMailSystemAnnouncement(obj) ? this.jsonResult(true, Constants.SENT_EMAIL_SUCCESS, obj) : this.jsonResult(false, Constants.SENT_EMAIL_ERROR, null);
 	}
 }
--- src/main/java/com/nwm/api/services/PlatformStatusService.java
+++ src/main/java/com/nwm/api/services/PlatformStatusService.java
@@ -5,10 +5,20 @@
 *********************************************************/
 package com.nwm.api.services;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 import com.nwm.api.DBManagers.DB;
+import com.nwm.api.entities.EmailAnnouncementRequest;
+import com.nwm.api.entities.EmployeeManageEntity;
 import com.nwm.api.entities.SystemAnnouncementEntity;
+import com.nwm.api.utils.Constants;
+import com.nwm.api.utils.Lib;
+import com.nwm.api.utils.SendMail;
 
 @Service
 public class PlatformStatusService extends DB {
@@ -38,6 +48,35 @@ public class PlatformStatusService extends DB {
 			return update("PlatformStatus.updateSystemAnnouncement", obj) > 0;
 		} catch (Exception ex) {
 			log.error("PlatformStatus.updateSystemAnnouncement", ex);
+			return false;
+		}
+	}
+	
+	/**
+	 * @description send mail system announcement
+	 * @author Hung.Bui
+	 * @since 2026-09-04
+	 * @param obj
+	 */
+	public boolean sendMailSystemAnnouncement(EmailAnnouncementRequest obj) {
+		try {
+			EmployeeService employeeService = new EmployeeService();
+			String subscribers = Optional.ofNullable((List<EmployeeManageEntity>) employeeService.getList(new EmployeeManageEntity()))
+					.map(data -> data.stream()
+							.filter(item -> item.getStatus() == 1 && item.getIs_delete() == 0)
+							.filter(item -> Objects.nonNull(item.getGroup_roles()))
+							.filter(item -> item.getGroup_roles().toLowerCase().contains("client"))
+							.map(EmployeeManageEntity::getEmail)
+							.collect(Collectors.joining(","))
+					)
+					.orElse("");
+			if (subscribers.isEmpty()) return false;
+			
+			String mailFromContact = Lib.getReourcePropValue(Constants.mailConfigFileName, Constants.mailFromContact);
+			
+			return SendMail.SendGmailTLS(mailFromContact,  "NEXT WAVE ENERGY MONITORING INC", "", "", subscribers, obj.getSubject(), obj.getMessage(), "system_announcement");
+		} catch (Exception ex) {
+			log.error("PlatformStatus.sendMailSystemAnnouncement", ex);
 			return false;
 		}
 	}
